c01ed6e5660e4385c3ee01798eb1892d066636d2
Anh Hoang Nguyen <iam@hoanganh.tech>
2022-03-21 22:02:21 -0400

    WIP events scaffolding

--- data/events.js
+++ data/events.js
@@ -1,7 +1,7 @@
 function createEvent({
     key = "",
     title = "",
-    description = "",
+    description = [],
     time = "",
     date = "",
     year = "",
@@ -16,4 +16,15 @@ function createEvent({
     };
 }
 
-export const events = [createEvent(), createEvent()];
+export const events = [
+    createEvent({
+        title: "The 2nd SAC Workshop",
+        description: [
+            "SAC workshop features research students' presentation, visiting scholars' seminar and demonstration of current research progress. The next SAC workshop is scheduled on Apr 3rd 2014 at the Faculty of Engineering and Computer Science, EV Building.",
+            "The next workshop will be held at the end of August. Contact Dr. Yan Liu for more information, #7255 or yan dot liu at concordia dot ca",
+        ],
+        date: "April 3",
+        year: "2014",
+    }),
+    createEvent(),
+];
--- pages/events.js
+++ pages/events.js
@@ -1,12 +1,22 @@
 import SEO from "../components/SEO";
 import Layout from "../components/Layout";
 import { getGlobalData } from "../utils/global-data";
+import { events } from "../data/events";
+
+function eventsMapping() {
+    return events.map((event, index) => (
+        <div key={index}>
+            <h2 className="text-2xl">{event.title}</h2>
+        </div>
+    ));
+}
 
 export default function Events() {
     const globalData = getGlobalData();
     return (
         <Layout>
             <SEO title="SAC | Events" description={globalData.description}></SEO>
+            {eventsMapping()}
         </Layout>
     );
 }

3cac0b5f0cb7d8b05adb4c85c1ab3324ab721a00
Anh Hoang Nguyen <iam@hoanganh.tech>
2022-04-09 16:06:48 -0400

    init homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import { getGlobalData } from "../utils/global-data";
+import Homepage from "../components/Homepage";
 
 export default function Home({ globalData }) {
     return (
         <Layout>
-            <SEO
-                title={globalData.siteName}
-                description={globalData.description}
-            ></SEO>
+            <SEO title={globalData.siteName} description={globalData.description}></SEO>
+            <div className="flex flex-col mt-8 mx-4 justify-center items-center">
+                <div>
+                    <h3 className="text-red-800 ">
+                        SAC research group focuses on data intensive applications in various domains
+                        running in and for the cloud
+                    </h3>
+                    <h4>
+                        Our research endeavours on designing adaptive software components,
+                        architecture framework and analysis method to scale data intensive
+                        distributed systems, through modeling software architecture; quantifying the
+                        system's characteristics using statistical models; predicting the system's
+                        quality attributes and devising system platforms. Current projects
+                        investigate mechanisms to improve the scalability and reliability of
+                        MapReduce Framework and Stream Processing Middleware to process incremental
+                        data analysis.
+                    </h4>
+                </div>
+            </div>
         </Layout>
     );
 }

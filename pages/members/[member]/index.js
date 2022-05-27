c58193454a776587c8f430596014fae37d16d265
Anh Hoang Nguyen <iam@hoanganh.tech>
2022-05-26 23:33:26 -0400

    WIP individual profile page

--- pages/members/[member]/index.js
+++ pages/members/[member]/index.js
@@ -6,9 +6,56 @@ export default function Member() {
     const router = useRouter();
     const { member } = router.query;
 
+    const getMemberObj = members.filter(
+        (obj) => (obj.firstName + obj.midName + obj.lastName).toLowerCase() == member
+    )[0];
+
     return (
         <Layout>
-            <h1>Member: {member}</h1>
+            <div className="m-16">
+                <div className="flex flex-col max-w-[300px] min-w-64 min-h-[600px] justify-center rounded-md shadow-md p-6 space-y-2">
+                    <div className="flex flex-col items-center justify-center">
+                        <div
+                            id="member-photo"
+                            style={{
+                                backgroundImage:
+                                    "url('/members-photos/" + getMemberObj.photo + "')",
+                            }}
+                            className={"w-48 h-48 flex-none bg-center bg-cover rounded-full"}
+                        ></div>
+                        <h1 className="text-2xl font-bold">
+                            {getMemberObj.firstName +
+                                " " +
+                                getMemberObj.midName +
+                                " " +
+                                getMemberObj.lastName}
+                        </h1>
+                    </div>
+
+                    <div className="flex flex-wrap justify-center space-x-2 items-center">
+                        <button className="border border-black rounded-sm p-1">LinkedIn</button>
+                        <button className="border border-black rounded-sm p-1">Github</button>
+                        <button className="border border-black rounded-sm p-1">Homepage</button>
+                    </div>
+                    <div className="flex flex-wrap justify-center flex-col items-center">
+                        <div id="education" className="">
+                            Concordia University
+                        </div>
+                        <div id="location" className="">
+                            Montreal, QC. Canada
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap justify-center flex-col">
+                        <h2 className="text-2xl font-bold">Interests</h2>
+                        <ul className="list-disc list-inside">
+                            <li>Machine Learning Operations</li>
+                            <li>Cloud Architectures</li>
+                            <li>System Designs</li>
+                            <li>SotA Software Frameworks</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </Layout>
     );
 }

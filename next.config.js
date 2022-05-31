a5129fbe299f1d16531fd46f3f3372dc768008da
Anh Hoang Nguyen <iam@hoanganh.tech>
2022-05-31 00:41:55 -0400

    docker is building but large image size

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
+    reactStrictMode: true,
 };
 
 const withTM = require("next-transpile-modules")(["three"]);
-
+module.exports = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    experimental: {
+        outputStandalone: true,
+    },
+};
 module.exports = withTM();
 module.exports = nextConfig;
